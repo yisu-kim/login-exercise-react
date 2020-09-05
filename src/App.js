c700cfd0c4be428d9fe5e6adb5a98d45c8b54dfd
yisu-kim <yisu.kim.dev@outlook.com>
2020-09-05 21:03:33 +0900

    [Feat] 라우트 설정

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import React from "react";
+import { Route } from "react-router-dom";
+import { Home, Auth } from "pages";
 
 function App() {
-  return <div className="App">Hello react</div>;
+  return (
+    <div className="App">
+      <Route exact path="/" component={Home} />
+      <Route path="/auth" component={Auth} />
+    </div>
+  );
 }
 
 export default App;
